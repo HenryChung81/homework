5fdd4b94d1cc32ad8cd5e643df6b61707861aa2c
Henry Chung <chungjenhao81@gmail.com>
2020-06-11 14:42:35 +0800

    homeworkdone

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 public class Tester {
 
     public static void main(String[] args) {
-        System.out.println("Please enter number of tickets: ");
-        Scanner scanner = new Scanner(System.in);
-        int tickets = scanner.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        int roundTrip = scanner.nextInt();
-        Ticket t = new Ticket(tickets,roundTrip);
-        t.printInfo();
+        int tickets = 0;
+        while (tickets != -1) {
+
+            System.out.println("Please enter number of tickets: ");
+            Scanner scanner = new Scanner(System.in);
+            tickets = scanner.nextInt();
+            if (tickets == -1) break;
+            System.out.println("How many round-trip tickets: ");
+            int roundTrip = scanner.nextInt();
+            Ticket t = new Ticket(tickets, roundTrip);
+            t.printInfo();
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,13 +3,18 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    println("Please enter number of tickets: ")
-    val scanner = Scanner(System.`in`)
-    val tickets = scanner.nextInt()
-    println("How many round-trip tickets: ")
-    val roundTrip = scanner.nextInt()
-    val t = Ticket1(tickets, roundTrip)
-    t.printInfo()
+    var tickets = 0
+
+    while (tickets != -1) {
+        println("Please enter number of tickets: ")
+        val scanner = Scanner(System.`in`)
+        tickets = scanner.nextInt()
+        if (tickets == -1) break;
+        println("How many round-trip tickets: ")
+        val roundTrip = scanner.nextInt()
+        val t = Ticket1(tickets, roundTrip)
+        t.printInfo()
+    }
 }
 
 class Ticket1(var tickets: Int, var roundTrip: Int) {
@@ -20,3 +25,5 @@ class Ticket1(var tickets: Int, var roundTrip: Int) {
         println("Total:" + ((tickets - roundTrip) * 1000 + ((roundTrip * 2000) * 0.9)))
     }
 }
+
+
